6cc62b8973b36cb53f2d576d157bb8f2d9c07b16
Zeeshan <Zeeshan@Zeeshan-PC>
2016-12-29 01:21:31 +0530

    First Time Upload

---
+++ GitHub/src/Login.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class Login {
+
+	/**
+	 * @param args
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+WebDriver drv = new FirefoxDriver();
+		drv.get("http://localhost:100");
+		drv.findElement(By.name("user_name")).sendKeys("admin");
+		drv.findElement(By.name("user_password")).sendKeys("admin");
+		Thread.sleep(10000);
+		drv.findElement(By.name("Login")).click();
+	}
+
+}
